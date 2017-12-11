0c9361f28e18f84190d4ce86dd0e42f163b5a482
Bohdan Lyzanets <bohdanl@wix.com>
2017-12-12 01:05:55 +0200

    2017/Day 10 (part 2)

--- 2017/d10/d10.js
+++ 2017/d10/d10.js
@@ -1,4 +1,4 @@
-export {part1, processList};
+export {part1, part2};
 
 function part1(list, length) {
   list = processList(list, length);
@@ -6,17 +6,66 @@ function part1(list, length) {
   return list[0] * list[1];
 }
 
-function processList(list, lengths) {
+function part2(input) {
+  return getKnotHash(input);
+}
+
+function getKnotHash(string) {
+  return getDenseHash(getSparseHash(string))
+    .reduce((acc, value) => {
+      const valueHash = value
+        .toString(16)
+        .padStart(2, 0);
+
+      return acc + valueHash;
+    }, '');
+}
+
+function getDenseHash(sparseHash) {
+  const denseHash = [];
+  const blocksCount = 16;
+  const blockSize = 16;
+
+  let blockIndex = 0;
+
+  while (blockIndex < blocksCount) {
+    denseHash.push(
+      sparseHash
+        .slice(
+          blockIndex * blockSize,
+          blockIndex * blockSize + blockSize
+        )
+        .reduce((sum, value) => sum ^ value, 0)
+    );
+
+    blockIndex++;
+  }
+
+  return denseHash;
+}
+
+function getSparseHash(string) {
+  const lengths = getSparseHashLength(string);
+  const list = Array.from(Array(256).keys());
+
+  return processList(list, lengths, 64)
+}
+
+function processList(list, lengths, rounds = 1) {
   let currentIndex = 0;
   let skipSize = 0;
 
-  lengths.forEach(length => {
-    list = reverseSublist(list, currentIndex, length);
+  while (rounds) {
+    lengths.forEach(length => {
+      list = reverseSublist(list, currentIndex, length);
 
-    currentIndex = (currentIndex + length + skipSize) % list.length;
+      currentIndex = (currentIndex + length + skipSize) % list.length;
 
-    skipSize++;
-  });
+      skipSize++;
+    });
+
+    rounds--;
+  }
 
   return list;
 }
@@ -25,6 +74,9 @@ function reverseSublist(list, startIndex, length) {
   const endIndex = startIndex + length - 1;
 
   let shift = 0;
+
+  // prevent mutation source list
+  list = [...list];
 
   while (shift < length / 2) {
     const indexA = (startIndex + shift) % list.length;
@@ -39,4 +91,11 @@ function reverseSublist(list, startIndex, length) {
   }
 
   return list;
+}
+
+function getSparseHashLength(string) {
+  return string
+    .split('')
+    .map(char => char.charCodeAt(0))
+    .concat([17, 31, 73, 47, 23])
 }
--- 2017/d10/d10.spec.js
+++ 2017/d10/d10.spec.js
@@ -1,20 +1,33 @@
-import {part1, processList} from './d10';
+import {part1, part2} from './d10';
 
 import inputSource from './d10.input.txt';
 
-describe('2017/Day 9', () => {
-  const inputArray = Array.from(Array(256).keys());
-  const inputLengths = inputSource
-    .split(',')
-    .map(value => parseInt(value, 10));
-
+describe('2017/Day 10', () => {
   describe('part1', () => {
+    const inputArray = Array.from(Array(256).keys());
+    const inputLengths = inputSource
+      .split(',')
+      .map(value => parseInt(value, 10));
+
     it('should resolve examples', () => {
-      expect(processList([0, 1, 2, 3, 4], [3, 4, 1, 5])).toEqual([3, 4, 2, 1, 0]);
       expect(part1([0, 1, 2, 3, 4], [3, 4, 1, 5])).toBe(12);
     });
     it('should resolve input', () => {
       expect(part1(inputArray, inputLengths)).toBe(23874);
+    });
+  });
+
+  describe('part1', () => {
+    const input = inputSource.trim();
+
+    it('should resolve examples', () => {
+      expect(part2('')).toEqual('a2582a3a0e66e6e86e3812dcb672a272');
+      expect(part2('AoC 2017')).toEqual('33efeb34ea91902bb2f59c9920caa6cd');
+      expect(part2('1,2,3')).toEqual('3efbe78a8d82f29979031a4aa0b16a9d');
+      expect(part2('1,2,4')).toEqual('63960835bcdc130f0b66d7ff4f6a5a8e');
+    });
+    it('should resolve input', () => {
+      expect(part2(input)).toEqual('e1a65bfb5a5ce396025fab5528c25a87');
     });
   });
 });
